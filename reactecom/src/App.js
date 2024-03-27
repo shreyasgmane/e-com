1883b4d52678ff4e928487eaf36ae4a9fd1bf5c2
hemshah2004@gmail.com <hemshah2004@gmail.com>
2024-03-27 21:28:49 +0530

    E-commerce Dashboard

---
+++ reactecom/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MasterLayout from './layouts/admin/MasterLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/admin" name="Admin" render={(props)=><MasterLayout {...props}/>}/>
+         
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactecom/src/layouts/admin/MasterLayout.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts';
+import routes from '../../routes/routes';
+
+
+const  MasterLayout = ()=> {
+  return (
+    <div className='sb-nav-fixed'>
+        <Navbar/>
+        <div id="layoutSidenav">
+            
+            <div id="layoutSidenav_nav">
+                    <Sidebar/>
+            </div>
+
+            <div id="layoutSidenav_content">
+                <main>
+                   <Routes>
+                        {routes.map((route,idx)=>{
+
+                          return (
+                            route.component &&(
+                              <Route
+                                key={idx}
+                                path={route.path}
+                                exact={route.exact}
+                                name={route.name}
+                                render={(props)=>(
+                                  <route.component {...props}/>
+                                )}
+                              />
+                            )
+                          )
+
+                        })}
+                        <Navigate to="/admin/dashboard"/>
+                   </Routes>
+                </main>
+                <Footer/>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default MasterLayout;
